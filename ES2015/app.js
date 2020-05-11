2ffd1840ab36c58f71cbcd356512717c8aad8531
gitadmin <gitadmin@muenai.com>
2020-05-11 17:54:49 +0800

    update

---
+++ ES2015/app.js
@@ -0,0 +1,10 @@
+
+
+
+let obj = {}
+obj.message = "hello world";
+obj.showMessage = function () {
+  alert(this.message);
+};
+
+// obj.showMessage()
---
+++ ES2015/quokka.js
@@ -0,0 +1,1 @@
+console.log([0, 0, 0].fill(2))
