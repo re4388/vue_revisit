1a035ffb167ad46a5d8c16ff7c825d44c26817ed
gitadmin <gitadmin@muenai.com>
2020-05-13 14:44:50 +0800

    add

--- ES2015/quokka.js
+++ ES2015/quokka.js
@@ -1,1 +1,17 @@
-console.log([0, 0, 0].fill(2))
+class SkinnedMesh extends THREE.Mesh {
+    constructor(geometry, materials) {
+        super(geometry, materials);
+
+        this.idMatrix = SkinnedMesh.defaultMatrix();
+        this.bones = [];
+        this.boneMatrices = [];
+        //...
+    }
+    update(camera) {
+        //...
+        super.update();
+    }
+    static defaultMatrix() {
+        return new THREE.Matrix4();
+    }
+}
--- TS_basic/readme.md
+++ TS_basic/readme.md
@@ -1,5 +1,8 @@
 
 
+## reference resources
+https://www.youtube.com/watch?v=ahCwqrYpIuM
+
 ## more resources
 
 Deep Dive https://github.com/basarat/typescript-book
